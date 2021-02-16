e1bfc3125984a0a1b9ca8582e2c06db6d1e6b149
p2rt@stagnationlab.com <p2rt@stagnationlab.com>
2021-02-16 13:34:22 +0200

    LoginView refactoring

--- client/src/components/loginForm/LogInForm.tsx
+++ client/src/components/loginForm/LogInForm.tsx
@@ -3,48 +3,43 @@ import "./login-form.css";
 import { useMutation, useLazyQuery, gql, useQuery } from "@apollo/client";
 import Paths from "../../Paths";
 import { Redirect, useHistory } from "react-router-dom";
+import { Field } from "../../components/Field/Field";
+import { useForm } from "react-hook-form";
+import { ReactComponent as EmailIcon } from "../../theme/icons/email-icon.svg";
+import { ReactComponent as PasswordIcon } from "../../theme/icons/password-icon.svg";
+import { validateEmail } from "../../validators/validateEmail";
+import { validateMinimumLength } from "../../validators/validateMinimumLength";
+
+interface LoginFormValues {
+  email: string;
+  password: string;
+}
 
 export default function LogInForm(props: any) {
+  const { register, handleSubmit, errors, watch } = useForm<LoginFormValues>();
   const history = useHistory();
+  const onSubmit = (data:any) => console.log(data);
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  async function handleSubmit(e: React.SyntheticEvent<HTMLFormElement>) {
-    e.preventDefault();
-    console.log("handleLoginFormSubmit");
-  }
 
   return (
-    <div className="login-form">
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <div className="form-row">
-          <label>E-mail address:</label>
-          <input
-            className="input"
-            type="email"
-            id="loginFormEmail"
-            name="loginFormEmail"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className="form-row">
-          <label>Password:</label>
-          <input
-            className="input"
-            type="password"
-            id="loginFormPassword"
-            name="loginFormPassword"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-
-        <button className="button" disabled={false} type="submit">
-          {"Log In"}
-        </button>
+      <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
+        <Field
+          type="email"
+          name="email"
+          label="Email"
+          leading={<EmailIcon />}
+          error={errors.email}
+          register={register({ required: "Email is required", validate: validateEmail })}
+        />
+        <Field
+          type="password"
+          name="password"
+          label="Password"
+          leading={<PasswordIcon />}
+          error={errors.password}
+          register={register({ validate: validateMinimumLength(8) })}
+        />
+        <input type="submit" />
       </form>
-    </div>
   );
 }
